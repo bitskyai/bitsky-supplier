b2fa299934ccb7c237523f049b136322deef770f
Shaoke <shaoke.xu@oracle.com>
2019-05-02 15:29:19 -0700

    Fix a bug that soi status doesn't update

--- src/apis/agenda/jobs/checkSOIStatus.js
+++ src/apis/agenda/jobs/checkSOIStatus.js
@@ -5,28 +5,38 @@ const {
 const {
     find
 } = require('../../../util/db');
-const {updateSOIStatus} = require('../../sois/helpers');
+const {
+    updateSOIStatus
+} = require('../../sois/helpers');
 const config = require('../../../config');
 const logger = require('../../../util/logger');
 
 module.exports = function (agenda) {
     agenda.define(jobNames.checkSOIStatus, async (job, done) => {
-        try{
+        try {
             // Get All SOIs
             // TODO: need to think about if you have million SOIs so you don't want to get all SOIs
             // for now assume sois doesn't too big
             let sois = await find(COLLECTIONS_NAME.sois, {
-                modified_at:{
-                    $lt: (Date.now()-config.SOI_STATUS_CHECK_TIME)
-                }
+                $or: [{
+                        modified_at: {
+                            $lt: (Date.now() - config.SOI_STATUS_CHECK_TIME)
+                        }
+                    },
+                    {
+                        modified_at: {
+                            $exists: false
+                        }
+                    }
+                ]
             });
 
             // TODO: This need to improve, it shouldn't check one by one, it should check multiple together
-            for(let i=0; i<sois.length; i++){
+            for (let i = 0; i < sois.length; i++) {
                 await updateSOIStatus(sois[i].global_id, sois[i]);
             }
             done();
-        }catch(err){
+        } catch (err) {
             logger.error(err);
         }
     });
