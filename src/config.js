a3b8af4f97328750ef6dff82f5de2a8daccb8272
Shaoke <shaokexu@gmail.com>
2020-01-05 22:32:56 -0800

    Add stop server function, and change the way of getConfig, previous implement has one issue, if process.env change, get config doesn't change

--- src/config.js
+++ src/config.js
@@ -1,40 +1,44 @@
 /* eslint-disable no-process-env */
 const constants = require("./util/constants");
 // Env vars should be casted to correct types
-let config = {
-  PORT: Number(process.env.PORT) || constants.CONFIG.PORT,
-  TYPEORM_CONNECTION: process.env.TYPEORM_CONNECTION,
-  TYPEORM_DATABASE: process.env.TYPEORM_DATABASE || constants.DEFAULT_SQLITE.database,
-  TYPEORM_URL: process.env.TYPEORM_URL || constants.DEFAULT_MONGODB.url,
-  TYPEORM_HOST: process.env.TYPEORM_HOST,
-  TYPEORM_PORT: process.env.TYPEORM_PORT,
-  TYPEORM_DATABASE: process.env.TYPEORM_DATABASE,
-  SOI_STATE_CHECK_TIME:
-    Number(process.env.SOI_STATE_CHECK_TIME) ||
-    constants.CONFIG.SOI_STATE_CHECK_TIME,
-  TIMEOUT_VALUE_FOR_INTELLIGENCE:
-    Number(process.env.TIMEOUT_VALUE_FOR_INTELLIGENCE) ||
-    constants.CONFIG.TIMEOUT_VALUE_FOR_INTELLIGENCE,
-  SERVICE_NAME: process.env.SERVICE_NAME || constants.CONFIG.SERVICE_NAME,
-  NODE_ENV: process.env.NODE_ENV || constants.CONFIG.NODE_ENV,
-  EACH_TIME_INTELLIGENCES_NUMBER:
-    process.env.EACH_TIME_INTELLIGENCES_NUMBER ||
-    constants.CONFIG.EACH_TIME_INTELLIGENCES_NUMBER,
-  LOG_FILES_PATH: process.env.LOG_FILES_PATH || constants.CONFIG.LOG_FILES_PATH,
-  LOG_LEVEL: process.env.LOG_LEVEL || constants.CONFIG.LOG_LEVEL,
-  DEBUG_MODE: process.env.DEBUG_MODE,
-  MONGODB_URI: process.env.MONGODB_URI || constants.CONFIG.MONGODB_URI,
-  DEFAULT_HEALTH_METHOD:
-    process.env.DEFAULT_HEALTH_METHOD || constants.CONFIG.DEFAULT_HEALTH_METHOD,
-  DEFAULT_HEALTH_PATH:
-    process.env.DEFAULT_HEALTH_PATH || constants.CONFIG.DEFAULT_HEALTH_PATH,
-  DEFAULT_INTELLIGENCES_METHOD:
-    process.env.DEFAULT_INTELLIGENCES_METHOD ||
-    constants.CONFIG.DEFAULT_INTELLIGENCES_METHOD,
-  DEFAULT_INTELLIGENCES_PATH:
-    process.env.DEFAULT_INTELLIGENCES_PATH ||
-    constants.CONFIG.DEFAULT_INTELLIGENCES_PATH
-};
+function getFullConfig(){
+  let config = {
+    PORT: Number(process.env.PORT) || constants.CONFIG.PORT,
+    TYPEORM_CONNECTION: process.env.TYPEORM_CONNECTION,
+    TYPEORM_DATABASE: process.env.TYPEORM_DATABASE || constants.DEFAULT_SQLITE.database,
+    TYPEORM_URL: process.env.TYPEORM_URL || constants.DEFAULT_MONGODB.url,
+    TYPEORM_HOST: process.env.TYPEORM_HOST,
+    TYPEORM_PORT: process.env.TYPEORM_PORT,
+    TYPEORM_DATABASE: process.env.TYPEORM_DATABASE,
+    SOI_STATE_CHECK_TIME:
+      Number(process.env.SOI_STATE_CHECK_TIME) ||
+      constants.CONFIG.SOI_STATE_CHECK_TIME,
+    TIMEOUT_VALUE_FOR_INTELLIGENCE:
+      Number(process.env.TIMEOUT_VALUE_FOR_INTELLIGENCE) ||
+      constants.CONFIG.TIMEOUT_VALUE_FOR_INTELLIGENCE,
+    SERVICE_NAME: process.env.SERVICE_NAME || constants.CONFIG.SERVICE_NAME,
+    NODE_ENV: process.env.NODE_ENV || constants.CONFIG.NODE_ENV,
+    EACH_TIME_INTELLIGENCES_NUMBER:
+      process.env.EACH_TIME_INTELLIGENCES_NUMBER ||
+      constants.CONFIG.EACH_TIME_INTELLIGENCES_NUMBER,
+    LOG_FILES_PATH: process.env.LOG_FILES_PATH || constants.CONFIG.LOG_FILES_PATH,
+    LOG_LEVEL: process.env.LOG_LEVEL || constants.CONFIG.LOG_LEVEL,
+    DEBUG_MODE: process.env.DEBUG_MODE,
+    MONGODB_URI: process.env.MONGODB_URI || constants.CONFIG.MONGODB_URI,
+    DEFAULT_HEALTH_METHOD:
+      process.env.DEFAULT_HEALTH_METHOD || constants.CONFIG.DEFAULT_HEALTH_METHOD,
+    DEFAULT_HEALTH_PATH:
+      process.env.DEFAULT_HEALTH_PATH || constants.CONFIG.DEFAULT_HEALTH_PATH,
+    DEFAULT_INTELLIGENCES_METHOD:
+      process.env.DEFAULT_INTELLIGENCES_METHOD ||
+      constants.CONFIG.DEFAULT_INTELLIGENCES_METHOD,
+    DEFAULT_INTELLIGENCES_PATH:
+      process.env.DEFAULT_INTELLIGENCES_PATH ||
+      constants.CONFIG.DEFAULT_INTELLIGENCES_PATH
+  };
+
+  return config;
+}
 
 /**
  * Overwrite config object. Change config have two way:
@@ -43,11 +47,13 @@ let config = {
  * @param {object} obj - config object that want to overwrite config
  */
 function overwriteConfig(obj) {
-  config = { ...config, ...obj };
-  return config;
+  // config = { ...config, ...obj };
+  // return config;
+  return getFullConfig();
 }
 
 function getConfig(key){
+  let config = getFullConfig();
   if(!key){
     return config;
   }else{
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ let { getConfig, overwriteConfig } = require("./config");
 const createApp = require("./app");
 import getDBConfiguration from "./util/dbConfiguration";
 
+//
+let dbConnection = null;
+let server = null;
+
 export async function startServer(customConfig) {
   try {
     if (customConfig) {
@@ -18,15 +22,20 @@ export async function startServer(customConfig) {
     logger.debug("startServer->config: ", getConfig());
     const dbConfig = getDBConfiguration();
     logger.debug(`dbConfig: %o `, dbConfig);
-    logger.debug(`typeorm: `, typeorm);
-    let connection = await typeorm.createConnection(dbConfig);
+    // if (dbConnection) {
+    //   dbConnection.close();
+    // }
+    dbConnection = await typeorm.createConnection(dbConfig);
     logger.debug("Create DB connection successfully.");
 
     const app = await createApp();
-    const server = app.listen(getConfig('PORT'), function() {
+    if (server) {
+      server.destroy();
+    }
+    server = app.listen(getConfig("PORT"), function() {
       logger.info(
         "Express server listening on http://localhost:%d/ in %s mode",
-        getConfig('PORT'),
+        getConfig("PORT"),
         app.get("env")
       );
     });
@@ -37,11 +46,13 @@ export async function startServer(customConfig) {
     process.on("SIGTERM", () => {
       logger.info(`SIGTERM received`);
       logger.info("Closing http.Server ..");
+      // dbConnection.close();
       server.destroy();
     });
     process.on("SIGINT", () => {
       logger.info(`SIGINT(Ctrl-C) received`);
       logger.info("Closing http.Server ..");
+      // dbConnection.close();
       server.destroy();
     });
 
@@ -51,9 +62,31 @@ export async function startServer(customConfig) {
 
       logger.info("Giving 100ms time to cleanup..");
       // Give a small time frame to clean up
-      setTimeout(process.exit, 100);
+      // setTimeout(process.exit, 100);
     });
   } catch (err) {
     throw err;
+  }
+}
+
+export async function stopServer() {
+  try {
+    // close dabtabase connection
+    dbConnection.close();
+    // close server
+    server.destroy();
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function testDBConnection(dbConfig) {
+  try {
+    dbConfig.name = "testConnection";
+    const connection = await typeorm.createConnection(dbConfig);
+    connection.close();
+    return true;
+  } catch (err) {
+    return false;
   }
 }
